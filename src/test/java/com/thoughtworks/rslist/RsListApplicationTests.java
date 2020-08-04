fd2df775c7d84b566be53cd3fddc7584b41783be
Ke Liu <ke.liu1@thoughtworks.com>
2020-08-04 23:26:57 +0800

    test

--- src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
+++ src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
@@ -1,13 +1,35 @@
 package com.thoughtworks.rslist;
 
+import com.thoughtworks.rslist.api.RsController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 class RsListApplicationTests {
 
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    RsController rsController;
+
     @Test
-    void contextLoads() {
+    void shouldGetOneRsEvent() throws Exception {
+     mockMvc.perform(get("/rs/1"))
+             .andExpect(content().string("第一条事件"))
+             .andExpect(status().isOk());
+     mockMvc.perform(get("/rs/2"))
+                .andExpect(content().string("第二条事件"))
+                .andExpect(status().isOk());
+     mockMvc.perform(get("/rs/3"))
+                .andExpect(content().string("第三条事件"))
+                .andExpect(status().isOk());
     }
 
 }
